3d58f33a94479037250640066e437b2399d1e4c1
Sangyoon98 <qlrqod123123@naver.com>
2025-07-11 18:47:51 +0900

    [FEAT] 프로젝트 기본 세팅, Tailwind

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { useState } from 'react'
-import './App.css'
+import * as React from "react";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Login from './Pages/Login';
+import Home from "./Pages/Home";
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App : React.FC = () => {
   return (
-    <>
-    </>
+    <Router>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/login" element={<Login/>} />
+      </Routes>
+    </Router>
   )
 }
 
