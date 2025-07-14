dad0c8f45d5648ab095edae40472af32935b6a99
Sangyoon98 <qlrqod123123@naver.com>
2025-07-15 00:50:35 +0900

    [FIX] TextField 클릭 사용성 개선 - 스크롤바 제거

--- src/components/InputField.tsx
+++ src/components/InputField.tsx
@@ -1,14 +1,9 @@
 import * as React from "react";
 import { useState } from "react";
-// @ts-ignore
 import MailIcon from "../assets/mail.svg?react";
-// @ts-ignore
 import PasswordIcon from "../assets/password.svg?react";
-// @ts-ignore
 import NicknameIcon from "../assets/nickname.svg?react";
-// @ts-ignore
 import Visible from "../assets/visibility_on.svg?react";
-// @ts-ignore
 import Invisible from "../assets/visibility_off.svg?react";
 
 interface InputFieldProps {
@@ -53,14 +48,14 @@ const InputField: React.FC<InputFieldProps> = ({
 
     return (
         <div className="mb-4 w-full">
-            <div className="flex items-center bg-white/80 rounded-full shadow-md text-xs px-4 py-3 mb-2 transition duration-200 focus-within:bg-white">
+            <div className="flex items-center bg-white/80 rounded-full shadow-md text-xs px-4 py-1 mb-2 transition duration-200 focus-within:bg-white">
                 {renderIcon()}
                 <input
                     type={showToggle && isVisible ? "text" : type}
                     placeholder={placeholder}
                     value={value !== undefined ? value : internalValue}
                     onChange={handleChange}
-                    className="ml-3 flex-1 bg-transparent outline-none text-black"
+                    className="ml-3 py-3 flex-1 bg-transparent outline-none text-black"
                 />
                 {showToggle && (
                     <button
--- src/components/home/CustomMovieSection.tsx
+++ src/components/home/CustomMovieSection.tsx
@@ -13,7 +13,7 @@ const CustomMovieSection: React.FC<CustomMovieSectionProps> = ({
             <h1 className="text-xl font-extralight mb-4 text-white">
                 맞춤 영화
             </h1>
-            <div className="flex gap-6 overflow-x-auto scrollbar-hide">
+            <div className="flex gap-6 overflow-x-hidden scrollbar-hide">
                 <Poster
                     posterImg={samplePoster}
                     posterName={"인사이드 아웃"}
