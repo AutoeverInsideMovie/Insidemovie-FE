38a50206a6bd3d657f5d6dcc0e8f20391ea42c31
Lee-Jong-Jin <ljj61111@gmail.com>
2025-07-15 11:01:32 +0900

    [FEAT] feat/#28 리뷰 작성 구현 시작 및 오타 수정/리팩터링_(250715_1100)

--- src/pages/BoxOffice.tsx
+++ src/pages/BoxOffice.tsx
@@ -1,16 +1,16 @@
 import * as React from "react";
 import { useState, useEffect } from "react";
 import axios from "axios";
-import TransparentBox from "../components/TransparentBox"; 
+import TransparentBox from "../components/TransparentBox";
 import Poster from "../assets/sample_poster.png";
 import StarRating from "../components/StarRating";
 
 interface Movie {
-  id: number;
-  rank: number;
-  title: string;
-  rating: number;
-  image: string;
+    id: number;
+    rank: number;
+    title: string;
+    rating: number;
+    image: string;
 }
 
 const BoxOffice: React.FC = () => {
@@ -31,46 +31,52 @@ const BoxOffice: React.FC = () => {
 
     return (
         <>
-          <div>
-            
-            <div className="flex justify-center">
-                <div className="max-w-screen-md w-full min-h-screen">
-                    <div className="flex flex-col">
-                        <div className="py-[240px]">
-                            <h1 className="text-white text-2xl font-semibold pb-6 text-left">
-                                박스오피스 순위 top100
-                            </h1>
-                            {movieList.map((movie)=>(
-                            <div key={movie.id} className="flex flex-row pb-6">
-                                <div className="text-4xl font-bold pl-8 first-line: pr-4 text-white">
-                                  {movie.rank}
-                                </div>
-                                <TransparentBox className="w-full h-[150px] overflow-hidden flex flex-row p-0">
-                                    <img
-                                        src={Poster/*movie.image*/}
-                                        alt="포스터"
-                                        className="h-full object-cover"
-                                    />
-                                    
-                                    <div className="p-4 flex flex-col justify-between">
-                                        <h1 className=" text-2xl font-sebold text-white ">
-                                          {movie.title}
-                                        </h1>
-                                        <div className="text-white flex flex-row">
-                                          <StarRating rating={movie.rating} /><div className="w-2 h-2"></div>{movie.rating.toFixed(1)}
+            <div>
+                <div className="flex justify-center">
+                    <div className="max-w-screen-md w-full min-h-screen">
+                        <div className="flex flex-col">
+                            <div className="py-[240px]">
+                                <h1 className="text-white text-2xl font-semibold pb-6 text-left">
+                                    박스오피스 순위 top100
+                                </h1>
+                                {movieList.map((movie) => (
+                                    <div
+                                        key={movie.id}
+                                        className="flex flex-row pb-6"
+                                    >
+                                        <div className="text-4xl font-bold pl-8 first-line: pr-4 text-white">
+                                            {movie.rank}
                                         </div>
-                                        <div className="flex flex-row text-white">
-                                          (감정 파라미터)
-                                        </div>
+                                        <TransparentBox className="w-full h-[150px] overflow-hidden flex flex-row p-0">
+                                            <img
+                                                src={Poster || movie.image}
+                                                alt="포스터"
+                                                className="h-full object-cover"
+                                            />
+
+                                            <div className="p-4 flex flex-col justify-between">
+                                                <h1 className=" text-2xl font-semibold text-white ">
+                                                    {movie.title}
+                                                </h1>
+                                                <div className="text-white flex flex-row">
+                                                    <StarRating
+                                                        rating={movie.rating}
+                                                    />
+                                                    <div className="w-2 h-2"></div>
+                                                    {movie.rating.toFixed(1)}
+                                                </div>
+                                                <div className="flex flex-row text-white">
+                                                    (감정 파라미터)
+                                                </div>
+                                            </div>
+                                        </TransparentBox>
                                     </div>
-                                </TransparentBox>
+                                ))}
                             </div>
-                           ))}
                         </div>
                     </div>
                 </div>
             </div>
-          </div>
         </>
     );
 };
