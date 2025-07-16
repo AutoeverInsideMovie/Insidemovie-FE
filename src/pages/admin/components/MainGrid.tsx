6a0287a28a59f1c4905e645b5d66d1044cad8f4b
Lee-jong-jin <ljj61111@gmail.com>
2025-07-16 17:57:29 +0900

    [FEAT] 관리자 페이지 일부 구현(WIP1)_(250716_1757)

--- src/pages/admin/components/MainGrid.tsx
+++ src/pages/admin/components/MainGrid.tsx
@@ -1,93 +1,95 @@
-import * as React from 'react';
-import Grid from '@mui/material/Grid';
-import Box from '@mui/material/Box';
-import Stack from '@mui/material/Stack';
-import Typography from '@mui/material/Typography';
-import Copyright from '../internals/components/Copyright';
-import ChartUserByCountry from './ChartUserByCountry';
-import CustomizedTreeView from './CustomizedTreeView';
-import CustomizedDataGrid from './CustomizedDataGrid';
-import HighlightedCard from './HighlightedCard';
-import PageViewsBarChart from './PageViewsBarChart';
-import SessionsChart from './SessionsChart';
-import StatCard, { StatCardProps } from './StatCard';
+import * as React from "react";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Stack from "@mui/material/Stack";
+import Typography from "@mui/material/Typography";
+import Copyright from "../internals/components/Copyright";
+import ChartUserByCountry from "./ChartUserByCountry";
+import CustomizedTreeView from "./CustomizedTreeView";
+import CustomizedDataGrid from "./CustomizedDataGrid";
+import HighlightedCard from "./HighlightedCard";
+import PageViewsBarChart from "./PageViewsBarChart";
+import SessionsChart from "./SessionsChart";
+import StatCard, { StatCardProps } from "./StatCard";
 
 const data: StatCardProps[] = [
-  {
-    title: 'Users',
-    value: '14k',
-    interval: 'Last 30 days',
-    trend: 'up',
-    data: [
-      200, 24, 220, 260, 240, 380, 100, 240, 280, 240, 300, 340, 320, 360, 340, 380,
-      360, 400, 380, 420, 400, 640, 340, 460, 440, 480, 460, 600, 880, 920,
-    ],
-  },
-  {
-    title: 'Conversions',
-    value: '325',
-    interval: 'Last 30 days',
-    trend: 'down',
-    data: [
-      1640, 1250, 970, 1130, 1050, 900, 720, 1080, 900, 450, 920, 820, 840, 600, 820,
-      780, 800, 760, 380, 740, 660, 620, 840, 500, 520, 480, 400, 360, 300, 220,
-    ],
-  },
-  {
-    title: 'Event count',
-    value: '200k',
-    interval: 'Last 30 days',
-    trend: 'neutral',
-    data: [
-      500, 400, 510, 530, 520, 600, 530, 520, 510, 730, 520, 510, 530, 620, 510, 530,
-      520, 410, 530, 520, 610, 530, 520, 610, 530, 420, 510, 430, 520, 510,
-    ],
-  },
+    {
+        title: "Users",
+        value: "3.2k",
+        interval: "Last 30 days",
+        trend: "up",
+        data: [
+            24, 100, 200, 220, 240, 240, 240, 260, 280, 300, 320, 340, 340, 340,
+            360, 360, 380, 380, 380, 400, 400, 420, 440, 460, 460, 480, 600,
+            640, 880, 920,
+        ],
+    },
+    {
+        title: "Reviews",
+        value: "325",
+        interval: "Last 30 days",
+        trend: "neutral",
+        data: [
+            220, 300, 360, 380, 400, 450, 480, 500, 520, 600, 620, 660, 720,
+            740, 760, 780, 800, 820, 820, 840, 840, 900, 900, 920, 970, 1050,
+            1080, 1130, 1250, 1640,
+        ],
+    },
+    {
+        title: "Reports",
+        value: "51",
+        interval: "Last 30 days",
+        trend: "down",
+        data: [
+            400, 410, 420, 430, 500, 510, 510, 510, 510, 510, 510, 520, 520,
+            520, 520, 520, 520, 530, 530, 530, 530, 530, 530, 530, 530, 600,
+            610, 610, 620, 730,
+        ],
+    },
 ];
 
 export default function MainGrid() {
-  return (
-    <Box sx={{ width: '100%', maxWidth: { sm: '100%', md: '1700px' } }}>
-      {/* cards */}
-      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
-        Overview
-      </Typography>
-      <Grid
-        container
-        spacing={2}
-        columns={12}
-        sx={{ mb: (theme) => theme.spacing(2) }}
-      >
-        {data.map((card, index) => (
-          <Grid key={index} size={{ xs: 12, sm: 6, lg: 3 }}>
-            <StatCard {...card} />
-          </Grid>
-        ))}
-        <Grid size={{ xs: 12, sm: 6, lg: 3 }}>
-          <HighlightedCard />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <SessionsChart />
-        </Grid>
-        <Grid size={{ xs: 12, md: 6 }}>
-          <PageViewsBarChart />
-        </Grid>
-      </Grid>
-      <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
-        Details
-      </Typography>
-      <Grid container spacing={2} columns={12}>
-        <Grid size={{ xs: 12, lg: 9 }}>
-          <CustomizedDataGrid />
-        </Grid>
-        <Grid size={{ xs: 12, lg: 3 }}>
-          <Stack gap={2} direction={{ xs: 'column', sm: 'row', lg: 'column' }}>
-            <CustomizedTreeView />
-            <ChartUserByCountry />
-          </Stack>
-        </Grid>
-      </Grid>
-      <Copyright sx={{ my: 4 }} />
-    </Box>
-  );
+    return (
+        <Box sx={{ width: "100%", maxWidth: { sm: "100%", md: "1700px" } }}>
+            {/* cards */}
+            <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+                Overview
+            </Typography>
+            <Grid
+                container
+                spacing={2}
+                columns={12}
+                sx={{ mb: (theme) => theme.spacing(2) }}
+            >
+                {data.map((card, index) => (
+                    <Grid key={index} size={{ xs: 12, sm: 6, lg: 4 }}>
+                        <StatCard {...card} />
+                    </Grid>
+                ))}
+                <Grid size={{ xs: 12, md: 6 }}>
+                    <SessionsChart />
+                </Grid>
+                <Grid size={{ xs: 12, md: 6 }}>
+                    <PageViewsBarChart />
+                </Grid>
+            </Grid>
+            <Typography component="h2" variant="h6" sx={{ mb: 2 }}>
+                Details
+            </Typography>
+            <Grid container spacing={2} columns={12}>
+                <Grid size={{ xs: 12, lg: 9 }}>
+                    <CustomizedDataGrid />
+                </Grid>
+                <Grid size={{ xs: 12, lg: 3 }}>
+                    <Stack
+                        gap={2}
+                        direction={{ xs: "column", sm: "row", lg: "column" }}
+                    >
+                        <CustomizedTreeView />
+                    </Stack>
+                </Grid>
+            </Grid>
+            <Copyright sx={{ my: 4 }} />
+        </Box>
+    );
 }
